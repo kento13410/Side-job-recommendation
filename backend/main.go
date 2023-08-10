c5008e2944da0a194da55c6a1200f12387755331
kento13410 <kento13410@gmail.com>
2023-08-10 13:31:35 +0900

    main->init, handler->func

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-    "net/http"
-    
-    "github.com/labstack/echo/v4"
+	"backend/handler"
+	_"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	handler.Init(e)
     e.Logger.Fatal(e.Start(":1323"))
 }
