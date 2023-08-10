d48dee2a7543c2b685bed6592b9d44324effc66b
kento13410 <kento13410@gmail.com>
2023-08-10 16:34:41 +0900

    まだ途中疲れたから別のブランチする

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,12 +1,10 @@
 CREATE TABLE IF NOT EXISTS `side_job` (
     `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     `name`	TEXT NOT NULL,
-    `description`	TEXT NOT NULL,
-    `image`	TEXT NOT NULL,
     `monthly_income`	INTEGER NOT NULL,
     `work_hours`	INTEGER NOT NULL,
     `skill_id`	INTEGER NOT NULL,
-    `difficulty`	TEXT NOT NULL,
+    `difficulty`	INTEGER NOT NULL,
     `category_id`	INTEGER NOT NULL
 );
 
