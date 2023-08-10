2998e4a131440b3f2c945b1caf6a29c360169729
kento13410 <kento13410@gmail.com>
2023-08-10 21:36:01 +0900

    schemaの変更、data.csvの追加

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,19 +1,15 @@
 CREATE TABLE IF NOT EXISTS `side_job` (
     `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     `name`	TEXT NOT NULL,
-    `monthly_income`	INTEGER NOT NULL,
-    `work_hours`	INTEGER NOT NULL,
+    `minimal_hourly_wage`	INTEGER NOT NULL,
+    `maximal_hourly_wage`	INTEGER NOT NULL,
+    `minmal_monthly_uptime`	INTEGER NOT NULL,
     `skill_id`	INTEGER NOT NULL,
-    `difficulty`	INTEGER NOT NULL,
-    `category_id`	INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS category (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name TEXT NOT NULL
+    `difficulty`	INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS skill (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name TEXT NOT NULL
+    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    `name` TEXT NOT NULL,
+    `side_job_id` INTEGER NOT NULL UNIQUE
 );
